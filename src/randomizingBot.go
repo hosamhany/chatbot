e88c5653f3ba3b1e2cf409d3c6f3a136ec22281c
hosamhany <7ossHany@gmail.com>
2018-01-22 13:22:38 +0200

    implemented the random all and pairs methods

--- src/randomizingBot.go
+++ src/randomizingBot.go
@@ -7,8 +7,11 @@ import (
   "os"
   "log"
 	"github.com/nlopes/slack"
+	"math/rand"
+	"time"
 )
 func main() {
+	rand.Seed(time.Now().Unix())
   err := godotenv.Load()
  if err != nil {
    log.Fatal("Error loading .env file")
@@ -36,9 +39,9 @@ Loop:
 				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
           fmt.Println(ev.User)
           var input = addIntoArray(ev.Text)
-          assignRandomType(input[1], rtm, ev)
+          assignRandomType(input[1], rtm, ev, input)
           // rtm.SendMessage(rtm.NewOutgoingMessage(randType, ev.Channel))
-					rtm.SendMessage(rtm.NewOutgoingMessage(erza3(input), ev.Channel))
+					// rtm.SendMessage(rtm.NewOutgoingMessage(erza3(input), ev.Channel))
 				}
 
 			case *slack.RTMError:
@@ -58,21 +61,44 @@ Loop:
     return strings.Split(text, " ")
   }
 
-  func assignRandomType(text string, rtm *slack.RTM, ev *slack.MessageEvent){
+  func assignRandomType(text string, rtm *slack.RTM, ev *slack.MessageEvent, input []string){
     switch text {
     case "randomPairs":
-      fmt.Println("randomPairs")
+			output:=shuffleAll(input, rtm, ev)
+			for i:= range output{
+				if(i%2==0 && i< len(output)-1){
+					rtm.SendMessage(rtm.NewOutgoingMessage(output[i]+" "+output[i+1], ev.Channel))
+					if(i == len(output)-2){
+						break
+					}
+				} else if(i == len(output)-1){
+					rtm.SendMessage(rtm.NewOutgoingMessage(output[i], ev.Channel))
+
+				}
+			}
     case "one":
-      fmt.Println("randomOne")
+			selectRandomOne(input, rtm, ev)
     case "all":
-      fmt.Println("randomAll")
+			output:=shuffleAll(input, rtm, ev)
+			rtm.SendMessage(rtm.NewOutgoingMessage(strings.Join(output," "), ev.Channel))
     default:
       rtm.SendMessage(rtm.NewOutgoingMessage("arza3", ev.Channel))
 
     }
   }
+	func selectRandomOne(input []string,rtm *slack.RTM, ev *slack.MessageEvent){
+		input = append(input[:0], input[2:]...)
+		rtm.SendMessage(rtm.NewOutgoingMessage(input[rand.Intn(len(input))], ev.Channel))
+	}
 
-  func erza3(data []string)string{
-    // RandType :=  data[1]
-    return data[0]
-  }
+	func shuffleAll(input []string,rtm *slack.RTM, ev *slack.MessageEvent) []string{
+		input = append(input[:0], input[2:]...)
+		for i:=range input{
+			fmt.Println(i)
+			j:= rand.Intn(i+1)
+			input[i], input[j] = input[j], input[i]
+		}
+		fmt.Println("INPUT" , input)
+		return input
+		// return strings.Join(input, " ")
+	}
